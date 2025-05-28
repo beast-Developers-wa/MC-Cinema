979934edf181c772c6c865dc494bf0a762360a81
KALEESWARANS-CYBER00 <skaleeswaran00@gmail.com>
2025-05-28 21:00:56 +0530

    Updated Signup page

--- client-side/src/pages/Signup.jsx
+++ client-side/src/pages/Signup.jsx
@@ -82,7 +82,7 @@ export default function Signup() {
     setSuccessMsg('');
 
     try {
-      const res = await fetch('http://192.168.43.16:5000/signup', {
+      const res = await fetch('http://your-ip:5000/signup', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, email, password, mobile }),
